fa11347f8d33cbf7e49d7950c3cdff5591f484fc
xjk <xjk@gmail.com>
2020-06-10 23:05:10 +0800

    to correct decode number

--- decodeString.py
+++ decodeString.py
@@ -44,6 +44,52 @@ class Solution:
 
 
 # 2020.06.10 
+# 对列与栈 --》 解码
+'''
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, res, multi = [], "", 0
+        for c in s:
+            if c == '[':
+                stack.append([multi, res])
+                res, multi = "", 0
+            elif c == ']':
+                cur_multi, last_res = stack.pop()
+                res = last_res + cur_multi*res
+
+            elif '0' <= c <= '9':
+                multi = multi * 10 + int(c)
+            else:
+                res += c
+        return res
+'''
+
+'''
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def dfs(s, i):
+            res, multi = "", 0
+            while i < len(s):
+                if '0' <= s[i] <= '9':
+                    multi = multi * 10 + int(s[i])
+                elif s[i] == '[':
+                    i, tmp = dfs(s, i + 1)
+                    res += multi * tmp
+                    multi = 0
+                elif s[i] == ']':
+                    return i, res
+                else:
+                    res += s[i]
+                i += 1
+            return res
+        return dfs(s, 0)
+'''
+
+
+
+
+
+# 2020.06.10 
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
@@ -63,8 +109,12 @@ class Solution:
                 c = stack.pop()
                 while c.isdigit():
                     times = c + times
-                    c = stack.pop()
-                stack.append(c)
+                    if len(stack) > 0:
+                        c = stack.pop()
+                    else:
+                        break
+                if not c.isdigit():
+                    stack.append(c)
                 local = int(times) * local
                 for c in local:
                     stack.append(c)
@@ -74,4 +124,6 @@ class Solution:
             c = stack.pop()
             result = c + result
         return result
+                
+
                 
