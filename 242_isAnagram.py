4dc91966a4d0b6ba4b5fdfdafb8d460a12827fb4
xjk <xjk@gmail.com>
2020-06-17 22:28:26 +0800

    using sort

--- 242_isAnagram.py
+++ 242_isAnagram.py
@@ -1,7 +1,7 @@
 '''
 histogram based on dict
 '''
-
+'''
 from collections import defaultdict
 
 class Solution:
@@ -15,4 +15,23 @@ class Solution:
             if v != 0:
                 return False
         return True
+'''
+
+'''
+sort
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        len_s = len(s)
+        len_t = len(t)
+        if len_s != len_t:
+            return False
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+
+        for i in range(len_s):
+            if sorted_s[i] != sorted_t[i]:
+                return False
+        return True
+
         
